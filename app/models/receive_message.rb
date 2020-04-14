a2205fbd6c58e826f8985be6d1958860e78999b8
EC2 Default User <ec2-user@ip-172-31-47-172.ap-northeast-1.compute.internal>
2020-04-14 10:11:03 +0000

    first commit

---
+++ app/models/receive_message.rb
@@ -0,0 +1,2 @@
+class ReceiveMessage < ApplicationRecord
+end
---
+++ db/migrate/20200413105948_create_receive_messages.rb
@@ -0,0 +1,12 @@
+class CreateReceiveMessages < ActiveRecord::Migration[5.2]
+  def change
+    create_table :receive_messages do |t|
+      t.string :user_id
+      t.string :reply_token
+      t.integer :message_id
+      t.string :message_type
+      t.text :message_text
+      t.timestamps
+    end
+  end
+end
